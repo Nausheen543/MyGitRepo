eacc0d9b5c7dd40a629c03115f0b7a126e096687
nausheen543 <nausheenmohammad543@gmail.com>
2016-07-01 15:20:22 +0530

    commit

---
+++ Helloworld/src/git/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package git;
+
+/**
+ * @author NAUSHEENMOHAMMAD
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello and Hi GIT!!!");
+	}
+
+}
---
+++ Helloworld/src/git/NewProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package git;
+
+/**
+ * @author NAUSHEENMOHAMMAD
+ *
+ */
+public class NewProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New program");
+	}
+
+}
